92feb8afa6665a9d80fe1c3adc3b24e6168bf6c8
superXR <270294379@qq.com>
2020-05-03 15:01:09 +0800

    解决value问题

--- src/pages/event/Event/SearchFilter/index.tsx
+++ src/pages/event/Event/SearchFilter/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Input, Button } from 'antd';
+import { Input, Button, Form } from 'antd';
 import styles from './index.scss';
 import { EventSearchProps } from '@/interfaces/event';
 
@@ -11,29 +11,24 @@ interface SearchFilterProps {
 
 export default class SearchFilter extends React.PureComponent<SearchFilterProps> {
   render() {
-    const { searchProps, onSearch, changeSearchProps } = this.props;
+    const { onSearch, changeSearchProps } = this.props;
     return (
 
       <div className={styles.filterPanel}>
-
         <div className={styles.filterItem}>
           <span className={styles.label}>比赛项目名称：</span>
+
           <Input
             allowClear={true}
-            value={searchProps.competitionEventName}
             placeholder="请输入比赛项目名称"
             onChange={e => changeSearchProps({ competitionEventName: e.target.value })}
           />
         </div>
-        <Button type="primary" onClick={onSearch}>
-          查询
-        </Button>
 
         <div className={styles.filterItem}>
           <span className={styles.label}>比赛项目编码：</span>
           <Input
             allowClear={true}
-            value={searchProps.competitionEventCode}
             placeholder="请输入比赛项目编码"
             onChange={e => changeSearchProps({ competitionEventCode: e.target.value })}
           />
